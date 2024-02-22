0d3c6adcf3a81c125a68420f80550f7964ad7f2e
Jong-Min <codejomo99@gmail.com>
2024-02-22 16:15:27 +0900

    Refactor

--- src/main/java/com/elice/post/controller/BoardController.java
+++ src/main/java/com/elice/post/controller/BoardController.java
@@ -1,10 +1,12 @@
 package com.elice.post.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
 
