92b19d5f74f0e0aeeb3c21f3a6dca9fd859d8793
Jong-Min <codejomo99@gmail.com>
2024-02-22 16:30:07 +0900

    Feat: Controller - CR구현

--- src/main/java/com/elice/post/controller/BoardController.java
+++ src/main/java/com/elice/post/controller/BoardController.java
@@ -1,13 +1,38 @@
 package com.elice.post.controller;
 
+import com.elice.post.dto.BoardDTO;
+import com.elice.post.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO){
+        boardService.save(boardDTO);
+        return "redirect:/board/paging";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        // 조회수 증가 ++
+
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board",boardDTO);
+        return "detail";
+    }
+
 
 }
--- src/main/java/com/elice/post/controller/HomeController.java
+++ src/main/java/com/elice/post/controller/HomeController.java
@@ -1,11 +1,27 @@
 package com.elice.post.controller;
 
+import com.elice.post.dto.BoardDTO;
+import com.elice.post.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/")
+    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model){
+//        pageable.getPageNumber();
+        return "paging";
+    }
 
 }
