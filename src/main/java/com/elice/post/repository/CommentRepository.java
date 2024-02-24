f247c9282a6bb06e9aaf05e1dd6d7861e7e5157e
Jong-Min <codejomo99@gmail.com>
2024-02-24 23:28:47 +0900

    Feat:댓글서비스 - Read 구현

--- src/main/java/com/elice/post/repository/CommentRepository.java
+++ src/main/java/com/elice/post/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.elice.post.repository;
 
+import com.elice.post.entity.BoardEntity;
 import com.elice.post.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findAllByBoardEntityOrderByIdDesc(BoardEntity boardEntity);
 }
--- src/main/java/com/elice/post/service/CommentService.java
+++ src/main/java/com/elice/post/service/CommentService.java
@@ -9,6 +9,8 @@ import com.elice.post.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,6 +32,21 @@ public class CommentService {
         }
 
     }
+
+    public List<CommentDTO> findAll(Long boardId){
+        BoardEntity boardEntity = boardRepository.findById(boardId).get();
+        List<CommentEntity> commentEntityList = commentRepository.findAllByBoardEntityOrderByIdDesc(boardEntity);
+
+        List<CommentDTO> commentDTOList = new ArrayList<>();
+        for(CommentEntity commentEntity : commentEntityList){
+            CommentDTO commentDTO = CommentDTO.toCommentDTO(commentEntity, boardId);
+            commentDTOList.add(commentDTO);
+        }
+        return commentDTOList;
+    }
+
+
+
 
     public void delete(Long id){
         commentRepository.deleteById(id);
