ffca90b2495e5cdf995308685511b44f215724e2
Jong-Min <codejomo99@gmail.com>
2024-02-24 23:13:11 +0900

    Feat:댓글 Entity,DTO

---
+++ src/main/java/com/elice/post/entity/CommentEntity.java
@@ -0,0 +1,41 @@
+package com.elice.post.entity;
+
+
+import com.elice.post.dto.CommentDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name="comment_tables")
+public class CommentEntity extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long commentPass;
+
+    @Column(length = 20, nullable = false)
+    private String commentWriter;
+
+    @Column
+    private String commentContents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_id")
+    private BoardEntity boardEntity;
+
+    public static CommentEntity toSaveEntity(CommentDTO commentDTO, BoardEntity boardEntity){
+        CommentEntity commentEntity = new CommentEntity();
+        commentEntity.setCommentWriter(commentDTO.getCommentWriter());
+        commentEntity.setCommentContents(commentDTO.getCommentContents());
+        commentEntity.setBoardEntity(boardEntity);
+        return commentEntity;
+    }
+
+
+}
