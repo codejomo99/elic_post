8b0e7188d40ddac65d615708838cd843dae8c4b5
Jong-Min <codejomo99@gmail.com>
2024-02-26 22:33:55 +0900

    Feat:파일첨부 Service 구현

--- src/main/java/com/elice/post/service/BoardService.java
+++ src/main/java/com/elice/post/service/BoardService.java
@@ -3,6 +3,7 @@ package com.elice.post.service;
 
 import com.elice.post.dto.BoardDTO;
 import com.elice.post.entity.BoardEntity;
+import com.elice.post.entity.BoardFileEntity;
 import com.elice.post.repository.BoardFileRepository;
 import com.elice.post.repository.BoardRepository;
 import jakarta.transaction.Transactional;
@@ -12,7 +13,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -22,11 +26,40 @@ import java.util.Optional;
 public class BoardService {
     private final BoardRepository boardRepository;
     private final BoardFileRepository boardFileRepository;
-    public void save(BoardDTO boardDTO) {
+    public void save(BoardDTO boardDTO) throws IOException {
+        // 파일 첨부 여부에 따라 로직 분리
+        if (boardDTO.getBoardFile().isEmpty()) {
+            // 첨부 파일 없음.
+            BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
+            boardRepository.save(boardEntity);
+        } else {
+            // 첨부 파일 있음.
+
+            /*
+                1. DTO에 담긴 파일을 꺼냄
+                2. 파일의 이름 가져옴
+                3. 서버 저장용 이름을 만듦
+                // 내사진.jpg => 839798375892_내사진.jpg
+                4. 저장 경로 설정
+                5. 해당 경로에 파일 저장
+                6. board_table에 해당 데이터 save 처리
+                7. board_file_table에 해당 데이터 save 처리
+             */
+            BoardEntity boardEntity = BoardEntity.toSaveFileEntity(boardDTO);
+            Long savedId = boardRepository.save(boardEntity).getId();
+            BoardEntity board = boardRepository.findById(savedId).get();
+            for(MultipartFile boardFile: boardDTO.getBoardFile()){
+                String originalFilename = boardFile.getOriginalFilename(); // 2.
+                String storedFileName = System.currentTimeMillis() + "_" + originalFilename; // 3.
+                String savePath = "/Users/codejomo99/springboot_img/" + storedFileName; // C:/springboot_img/9802398403948_내사진.jpg
+                boardFile.transferTo(new File(savePath)); // 5.
+                BoardFileEntity boardFileEntity = BoardFileEntity.toBoardFileEntity(board, originalFilename, storedFileName);
+                boardFileRepository.save(boardFileEntity);
+            }
 
 
-        BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
-        boardRepository.save(boardEntity);
+        }
+
     }
 
 
