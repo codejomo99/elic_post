fa6a756538c42e8bf5733a2e0dd7d289d1cfe2fe
Jong-Min <codejomo99@gmail.com>
2024-02-26 22:28:34 +0900

    Feat:파일첨부 entity 구현

---
+++ src/main/java/com/elice/post/repository/BoardFileRepository.java
@@ -0,0 +1,7 @@
+package com.elice.post.repository;
+
+import com.elice.post.entity.BoardFileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardFileRepository extends JpaRepository<BoardFileEntity,Long> {
+}
