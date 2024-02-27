590389ddcfc0a82150e0871bd63abf92bd84dce4
Jong-Min <codejomo99@gmail.com>
2024-02-27 14:11:23 +0900

    Fix: keyword 폼,레포

--- src/main/java/com/elice/post/repository/BoardRepository.java
+++ src/main/java/com/elice/post/repository/BoardRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<BoardEntity,Long> {
 
     @Modifying
     @Query(value = "update BoardEntity b set b.boardHits=b.boardHits+1 where b.id=:id")
     void updateHits(@Param("id") Long id);
+
+
+    List<BoardEntity> findByBoardTitleContaining(String keyword);
+
 }
